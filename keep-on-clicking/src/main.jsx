bbe0129195561c64adae303e57f140e4dcba5e34
nicky-mc <cinimod@gmail.com>
2024-10-05 16:22:35 +0100

    updated code to be worked on

--- keep-on-clicking/src/main.jsx
+++ keep-on-clicking/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/global.css";
+import "./styles/Global.css";
 
 // Create a root for React to render into
 ReactDOM.createRoot(document.getElementById("root")).render(
