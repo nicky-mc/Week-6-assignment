48911239222689334c8760448f87f741c8d3187e
nicky-mc <cinimod@gmail.com>
2024-10-06 12:17:37 +0100

    component for rendering animated background and associated css completed

---
+++ keep-on-clicking/src/components/AnimatedBackground.jsx
@@ -0,0 +1,11 @@
+import "react";
+import "./AnitmatedBackground.css";
+
+const AnimatedBackground = () => {
+  return (
+    <div className="animated-background">
+      <div className="overlay"></div>
+    </div>
+  );
+};
+export default AnimatedBackground;
