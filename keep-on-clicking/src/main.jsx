c203f95da75de25124a15a0b0b0b954e66c89220
nicky-mc <cinimod@gmail.com>
2024-10-06 12:56:43 +0100

    app.css completed

--- keep-on-clicking/src/main.jsx
+++ keep-on-clicking/src/main.jsx
@@ -1,6 +1,7 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
